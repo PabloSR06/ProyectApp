a2cd0bec1e62405894926baff4f3b4487af44011
PabloSR06 <47283527+PabloSR06@users.noreply.github.com>
2021-10-27 21:45:06 +0100

    maps and alert system

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 
     <div>
-        <h1>Bienvenido1</h1>
+        <h1>Bienvenido</h1>
         <?php
             session_start();
             print_r($_SESSION);
@@ -50,7 +50,7 @@
                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                     </svg>
                 </a>
-                <a href="/proyectapp/pages/" class='nav_link'>
+                <a href="/proyectapp/pages/maps.html" class='nav_link'>
                     <svg class="nav_img" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                         <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
                         <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
