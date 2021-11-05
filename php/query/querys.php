ba0c046f69ff46955611ad250505555e259c59e0
PabloSR06 <47283527+PabloSR06@users.noreply.github.com>
2021-11-05 20:28:48 +0000

    Create querys.php

---
+++ php/query/querys.php
@@ -0,0 +1,52 @@
+<?php
+
+
+    function query_cars($db, $idClient){
+        try {
+            $sql = $db->prepare("SELECT * FROM cars WHERE idClient = :idClient");   
+
+            $sql->bindValue(':idClient', $idClient);
+            $sql->execute();
+
+            $myJSON = "{";
+            $myJSON .= '"coches":[';
+
+            $i = 0;
+            $row = $sql->fetchAll();
+            $count = count($row);
+
+            while ($i < $count) {
+                $myJSON .= '{';
+
+                $myJSON .= '"licensePlate":';
+                $myJSON .= '"' . $row[$i]['licensePlate'] . '"';
+
+                $myJSON .= ',';
+
+                $myJSON .= '"brand":';
+                $myJSON .= '"' . $row[$i]['brand'] . '"';
+                
+                $myJSON .= ',';
+
+                $myJSON .= '"model":';
+                $myJSON .= '"' . $row[$i]['model'] . '"';
+
+                $myJSON .= '}';
+
+                if (!($i == $count - 1)) {
+                    $myJSON .= ',';
+                }
+
+                $i++;
+            }
+
+            $myJSON .= '],';
+            $myJSON .= '"success":' . $i;
+            $myJSON .= '}';
+            //echo $myJSON;
+            return $myJSON;
+        } catch (\Throwable $th) {
+        }
+        
+    }
+?>
