be34855217e0cc453835a74d91213bace07fb7cc
Pablo <47283527+PabloSR06@users.noreply.github.com>
2021-12-16 18:21:23 +0000

    contant css fix

--- phpinfo.php
+++ phpinfo.php
@@ -1,2 +1,23 @@
-<?php phpinfo(); ?>
+<?php  
+
+
+
+
+
+try { 
+    require './php/query/querys.php';
+    require './php/conectDB.php';
+    $db = conectDB();
+    
+    $json =  query_cars2($db, 1);
+    
+    //$array = json_decode($json);
+    echo($json);
+} catch (\Throwable $th) {
+    echo "A ocurrido un error";
+}
+
+
+
+?>
 
